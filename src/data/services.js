c959571f8fc6c31edddcb942a3c7e4adc7a2fff7
Eynar Alvarez <eynar.j.alvarez.m@gmail.com>
2023-07-25 13:02:37 -0400

    h2 fixed

--- src/data/services.js
+++ src/data/services.js
@@ -4,8 +4,8 @@ export const services = [
         title: 'Seguridad Industrial',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis tortor ex. Fusce varius fringilla mauris, eget porttitor libero rhoncus id. Cras non justo justo. Nullam rhoncus libero ac malesuada dapibus. Donec vel elementum justo. Duis nisl ante, aliquam vel semper eget, malesuada ut risus. Mauris velit mauris, faucibus sit amet viverra id, pulvinar vel elit. Sed nec aliquet est, eget tempor mauris. Praesent blandit fringilla vulputate. Vivamus bibendum nisl diam, nec volutpat libero feugiat at. Pellentesque feugiat purus sem, a ultrices nulla rutrum sit amet. Nulla fermentum nunc lorem, in varius urna tristique at. Maecenas finibus auctor nulla, vitae posuere sem. Maecenas nec molestie tortor.',
         img1: "/img/bg01.jpeg",
+        titleDescription: "Consultoría",
         subTitle: {
-            h2: "Consultoría",
             p1: "Servicio 1",
             p2: "Servicio 2",
             p3: "Servicio 3",
@@ -18,8 +18,8 @@ export const services = [
         title: 'Consultoría y Construcción',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis tortor ex. Fusce varius fringilla mauris, eget porttitor libero rhoncus id. Cras non justo justo. Nullam rhoncus libero ac malesuada dapibus. Donec vel elementum justo. Duis nisl ante, aliquam vel semper eget, malesuada ut risus. Mauris velit mauris, faucibus sit amet viverra id, pulvinar vel elit. Sed nec aliquet est, eget tempor mauris. Praesent blandit fringilla vulputate. Vivamus bibendum nisl diam, nec volutpat libero feugiat at. Pellentesque feugiat purus sem, a ultrices nulla rutrum sit amet. Nulla fermentum nunc lorem, in varius urna tristique at. Maecenas finibus auctor nulla, vitae posuere sem. Maecenas nec molestie tortor.',
         img1: "/img/bg01.jpeg",
+        titleDescription: "Consultoría",
         subTitle: {
-            h2: "Consultoría",
             p1: "Servicio 1",
             p2: "Servicio 2",
             p3: "Servicio 3",
