8c9cd6eb36142f40501a381bb3decd5e00b19164
Eynar Alvarez <eynar.j.alvarez.m@gmail.com>
2023-07-25 12:12:26 -0400

    Services template was added

--- src/data/services.js
+++ src/data/services.js
@@ -2,31 +2,53 @@ export const services = [
     {
         id: 'seguridad-industrial',
         title: 'Seguridad Industrial',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis tortor ex. Fusce varius fringilla mauris, eget porttitor libero rhoncus id. Cras non justo justo. Nullam rhoncus libero ac malesuada dapibus. Donec vel elementum justo. Duis nisl ante, aliquam vel semper eget, malesuada ut risus. Mauris velit mauris, faucibus sit amet viverra id, pulvinar vel elit. Sed nec aliquet est, eget tempor mauris. Praesent blandit fringilla vulputate. Vivamus bibendum nisl diam, nec volutpat libero feugiat at. Pellentesque feugiat purus sem, a ultrices nulla rutrum sit amet. Nulla fermentum nunc lorem, in varius urna tristique at. Maecenas finibus auctor nulla, vitae posuere sem. Maecenas nec molestie tortor.',
+        img1: "/img/bg01.jpeg",
+        subTitle: {
+            h2: "Consultoría",
+            p1: "Servicio 1",
+            p2: "Servicio 2",
+            p3: "Servicio 3",
+            p4: "Servicio 4"
+        },
         img: "/img/bg02.jpg"
     },
     {
         id: 'consultoria-construccion',
         title: 'Consultoría y Construcción',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis tortor ex. Fusce varius fringilla mauris, eget porttitor libero rhoncus id. Cras non justo justo. Nullam rhoncus libero ac malesuada dapibus. Donec vel elementum justo. Duis nisl ante, aliquam vel semper eget, malesuada ut risus. Mauris velit mauris, faucibus sit amet viverra id, pulvinar vel elit. Sed nec aliquet est, eget tempor mauris. Praesent blandit fringilla vulputate. Vivamus bibendum nisl diam, nec volutpat libero feugiat at. Pellentesque feugiat purus sem, a ultrices nulla rutrum sit amet. Nulla fermentum nunc lorem, in varius urna tristique at. Maecenas finibus auctor nulla, vitae posuere sem. Maecenas nec molestie tortor.',
+        img1: "/img/bg01.jpeg",
+        subTitle: {
+            h2: "Consultoría",
+            p1: "Servicio 1",
+            p2: "Servicio 2",
+            p3: "Servicio 3",
+            p4: "Servicio 4"
+        },
         img: "/img/bg01.jpeg"
     },
     {
         id: 'marketing',
         title: 'Marketing',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis tortor ex. Fusce varius fringilla mauris, eget porttitor libero rhoncus id. Cras non justo justo. Nullam rhoncus libero ac malesuada dapibus. Donec vel elementum justo. Duis nisl ante, aliquam vel semper eget, malesuada ut risus. Mauris velit mauris, faucibus sit amet viverra id, pulvinar vel elit. Sed nec aliquet est, eget tempor mauris. Praesent blandit fringilla vulputate. Vivamus bibendum nisl diam, nec volutpat libero feugiat at. Pellentesque feugiat purus sem, a ultrices nulla rutrum sit amet. Nulla fermentum nunc lorem, in varius urna tristique at. Maecenas finibus auctor nulla, vitae posuere sem. Maecenas nec molestie tortor.',
         img: "/img/bg01.jpeg"
     },
     {
         id: 'diseno-grafico',
         title: 'Diseño Gráfico',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis tortor ex. Fusce varius fringilla mauris, eget porttitor libero rhoncus id. Cras non justo justo. Nullam rhoncus libero ac malesuada dapibus. Donec vel elementum justo. Duis nisl ante, aliquam vel semper eget, malesuada ut risus. Mauris velit mauris, faucibus sit amet viverra id, pulvinar vel elit. Sed nec aliquet est, eget tempor mauris. Praesent blandit fringilla vulputate. Vivamus bibendum nisl diam, nec volutpat libero feugiat at. Pellentesque feugiat purus sem, a ultrices nulla rutrum sit amet. Nulla fermentum nunc lorem, in varius urna tristique at. Maecenas finibus auctor nulla, vitae posuere sem. Maecenas nec molestie tortor.',
         img: "/img/bg02.jpg"
     },
     {
         id: 'contaduria',
         title: 'Contaduría',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis tortor ex. Fusce varius fringilla mauris, eget porttitor libero rhoncus id. Cras non justo justo. Nullam rhoncus libero ac malesuada dapibus. Donec vel elementum justo. Duis nisl ante, aliquam vel semper eget, malesuada ut risus. Mauris velit mauris, faucibus sit amet viverra id, pulvinar vel elit. Sed nec aliquet est, eget tempor mauris. Praesent blandit fringilla vulputate. Vivamus bibendum nisl diam, nec volutpat libero feugiat at. Pellentesque feugiat purus sem, a ultrices nulla rutrum sit amet. Nulla fermentum nunc lorem, in varius urna tristique at. Maecenas finibus auctor nulla, vitae posuere sem. Maecenas nec molestie tortor.',
         img: "/img/bg01.jpeg"
     },
     {
         id: 'contabilidad',
         title: 'Contabilidad',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis tortor ex. Fusce varius fringilla mauris, eget porttitor libero rhoncus id. Cras non justo justo. Nullam rhoncus libero ac malesuada dapibus. Donec vel elementum justo. Duis nisl ante, aliquam vel semper eget, malesuada ut risus. Mauris velit mauris, faucibus sit amet viverra id, pulvinar vel elit. Sed nec aliquet est, eget tempor mauris. Praesent blandit fringilla vulputate. Vivamus bibendum nisl diam, nec volutpat libero feugiat at. Pellentesque feugiat purus sem, a ultrices nulla rutrum sit amet. Nulla fermentum nunc lorem, in varius urna tristique at. Maecenas finibus auctor nulla, vitae posuere sem. Maecenas nec molestie tortor.',
         img: "/img/bg01.jpeg"
     }
 ]
